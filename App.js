eb4c1ece0a73c834dee628bd519be2b489c622ad
Chandiran <chandiranvc2001@gmail.com>
2023-11-06 15:53:54 +0530

    project on 6th nov

--- App.js
+++ App.js
@@ -1,1 +1,10 @@
-const f = 10;
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+const Heading = () =>(
+    <div>
+        <h1>Hello from Functional Component</h1>
+    </div>
+);
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<Heading/>);
